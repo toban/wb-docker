eb24b04d242fa4791ea40cfaebf86b71cd7d64e4
toan <tobias.andersson@wikimedia.de>
2021-03-12 11:19:24 +0100

    add wikibasehost

--- LocalSettings/LocalSettings.php
+++ LocalSettings/LocalSettings.php
@@ -3,7 +3,7 @@
 $wgVirtualRestConfig['modules']['parsoid'] = array(
     // URL to the Parsoid instance.
     // You should change $wgServer to match the non-local host running Parsoid
-    'url' => $wgServer . $wgScriptPath . '/rest.php',
+    'url' => 'http://' . $_ENV['WIKIBASE_HOST'] . $wgScriptPath . '/rest.php',
     // Parsoid "domain", see below (optional, rarely needed)
     // 'domain' => 'localhost',
 );
