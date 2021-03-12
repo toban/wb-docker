3df583d7596993cd0d5f1df5a5d5bba719a3e90d
toan <tobias.andersson@wikimedia.de>
2021-03-12 11:27:19 +0100

    add fedprops, add debug thing

--- LocalSettings/LocalSettings.php
+++ LocalSettings/LocalSettings.php
@@ -1,5 +1,18 @@
 <?php
 
+# Federated Properties
+$wgWBRepoSettings['federatedPropertiesEnabled'] = true;
+
+# simulate source API errors via request header
+if( isset ( $_SERVER['HTTP_X_FEDPROPS_SOURCEAPIRESPONSE'] ) ) {
+   $wgParserCacheType = CACHE_NONE;
+   $code = $_SERVER['HTTP_X_FEDPROPS_SOURCEAPIRESPONSE'];
+
+   if( $code == 404 ) {
+      $wgWBRepoSettings['federatedPropertiesSourceScriptUrl'] = 'http://notFound.404';
+   }
+}
+
 $wgVirtualRestConfig['modules']['parsoid'] = array(
     // URL to the Parsoid instance.
     // You should change $wgServer to match the non-local host running Parsoid
